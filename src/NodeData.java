d20a631a9c960ca0593a625ef6958403c110cecb
saar95 <saar9550@gmail.com>
2020-11-26 12:14:35 +0200

    directed weighted graph v1.0

---
+++ src/NodeData.java
@@ -0,0 +1,48 @@
+public class NodeData implements node_data{
+    private int key;
+
+    @Override
+    public int getKey() {
+        return 0;
+    }
+
+    @Override
+    public geo_location getLocation() {
+        return null;
+    }
+
+    @Override
+    public void setLocation(geo_location p) {
+
+    }
+
+    @Override
+    public double getWeight() {
+        return 0;
+    }
+
+    @Override
+    public void setWeight(double w) {
+
+    }
+
+    @Override
+    public String getInfo() {
+        return null;
+    }
+
+    @Override
+    public void setInfo(String s) {
+
+    }
+
+    @Override
+    public int getTag() {
+        return 0;
+    }
+
+    @Override
+    public void setTag(int t) {
+
+    }
+}
---
+++ src/geoLocation.java
@@ -0,0 +1,21 @@
+public class geoLocation implements geo_location {
+    @Override
+    public double x() {
+        return 0;
+    }
+
+    @Override
+    public double y() {
+        return 0;
+    }
+
+    @Override
+    public double z() {
+        return 0;
+    }
+
+    @Override
+    public double distance(geo_location g) {
+        return 0;
+    }
+}
