005639d41df18415d5dcd251a823e0ae411b6ef3
saar95 <saar9550@gmail.com>
2020-12-09 16:59:38 +0200

    Statring work on ex2 game

--- src/gameClient/Ex2.java
+++ src/gameClient/Ex2.java
@@ -11,9 +11,7 @@ public class Ex2 {
         directed_weighted_graph g1 = new DWGraph_DS();
         dw_graph_algorithms startGraph = new DWGraph_Algo();
         startGraph.init(g1);
-        startGraph.load("A0");
+        startGraph.load("Data/A0");
         System.out.println("b");
-
-
     }
 }
