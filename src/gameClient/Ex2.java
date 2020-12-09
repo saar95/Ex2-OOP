9f5be59d9b1d77c1bb87b82fb437f587662c52dd
saar95 <saar9550@gmail.com>
2020-12-09 19:22:18 +0200

    Ex2 game

--- src/gameClient/Ex2.java
+++ src/gameClient/Ex2.java
@@ -2,55 +2,51 @@ package gameClient;
 
 import Server.Game_Server_Ex2;
 import api.*;
-import com.google.gson.*;
-import com.google.gson.JsonObject;
 
 import java.util.Iterator;
+import java.util.List;
 
 
 public class Ex2 {
 
-    public static node_data[] loadPokemon(String pLocation,dw_graph_algorithms graph){
-        GsonBuilder gsonBuilder = new GsonBuilder();
-        int counter=0;
-        node_data minNode=null;
-        JsonObject pObject=new JsonObject();
-        JsonArray pokemonArray = pObject.get("Pokemons").getAsJsonArray();
-        for (JsonElement nodes : pokemonArray) {
-            JsonObject pokemonObject = nodes.getAsJsonObject();
-            counter++;
-        }
-       node_data[] arr=new node_data[counter];
-        for (JsonElement nodes : pokemonArray) {
-            JsonObject pokemonObject = nodes.getAsJsonObject();
-            String pos = pokemonObject.get("pos").getAsString();
-            String[] posArray = pos.split(",");
-            geo_location geo=new geoLocation(Double.parseDouble(posArray[0]),Double.parseDouble(posArray[1]),Double.parseDouble(posArray[2]));
-            double minDist=Double.MAX_VALUE;
-            for (int i=0;i<arr.length;i++){
-                Iterator<node_data> it1 = graph.getGraph().getV().iterator();
-                while (it1.hasNext()){
-                    node_data tempNode= it1.next();
-                    double dist=geo.distance(tempNode.getLocation());
-                    if(dist<minDist) {
-                        minDist = dist;
-                        minNode=tempNode;
-                    }
+    public static node_data[] loadPokemon(List<CL_Pokemon> l,dw_graph_algorithms graph){
+        node_data [] arr = new node_data[l.size()];
+        node_data temp=null;
+        Iterator<node_data> it = graph.getGraph().getV().iterator();
+        for (int i=0;i<l.size();i++){
+            double minDist =Double.MAX_VALUE;
+            while(it.hasNext()) {
+            temp = it.next();
+                if(temp.getLocation().distance(l.get(i).getLocation())<minDist) {
+                minDist = temp.getLocation().distance(l.get(i).getLocation());
+                arr[i] = temp;
                 }
-                arr[i]=minNode;
             }
         }
         return arr;
     }
     public static void main(String[] args) {
         int level_number=0;
+        int countAgent=0;
         game_service game = Game_Server_Ex2.getServer(level_number);
         directed_weighted_graph g1 = new DWGraph_DS();
         dw_graph_algorithms startGraph = new DWGraph_Algo();
         startGraph.init(g1);
         startGraph.load("Data/A0");
-        loadPokemon(game.getPokemons(), startGraph);
-        System.out.println("s");
-
+        List<CL_Pokemon> pokemonList = Arena.json2Pokemons(game.getPokemons());
+        List<CL_Agent> agentList = Arena.getAgents(game.getAgents(),startGraph.getGraph());
+        if(pokemonList.size()>agentList.size()) {
+            for (int i = 0; i <agentList.size(); i++) {
+                game.addAgent(loadPokemon(pokemonList, startGraph)[i].getKey());
+            }
+        }
+        else {
+            for (int i = 0; i < pokemonList.size(); i++) {
+                game.addAgent(loadPokemon(pokemonList, startGraph)[i].getKey());
+                countAgent++;
+            }
+            for (int i = countAgent;i<agentList.size();i++)
+                game.addAgent(i);
+        }
     }
 }
